2a10d99c9c6bcbee4d94c9248546cded104af637
Ernest <musaubrian15@gmail.com>
2023-03-01 11:47:04 +0300

    chore: improve ui
    * Increase font size of textbox font
    * Add quit button
    * Make quit button red

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ from tkinter import *
 from tkinter import filedialog
 
 root = Tk()
-root.title("Text Editor")
+root.title("Editr")
 
 text = Text(root)
-text.grid(column=1, row=1, rowspan=3)
+text.grid(column=1, row=1, rowspan=3, columnspan=2)
+text.configure(font="arial 15")
 
 
 def SaveAs():
     global text
     t = text.get("1.0", "end-1c")
     saveLocation = filedialog.asksaveasfilename()
-    file = open(saveLocation, "w+")
+    file = open(saveLocation, "w")
     file.write(t)
     file.close()
 
 
-button = Button(root, text="Save", font="arial 18", command=SaveAs)
-button.grid(column=1, row=4)
+save_button = Button(root, text="Save", font="arial 20", command=SaveAs)
+save_button.grid(column=1, row=4)
+quit_button = Button(root, text="Quit", font="arial 20", command=quit)
+quit_button.grid(column=2, row=4)
+quit_button.configure(foreground="red")
 
 
 root.mainloop()
